b9c290edf3c47935a13c0e543fd7c39a1da472d0
Tail0n <132779291+Tail0n@users.noreply.github.com>
2023-05-14 19:21:03 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,26 @@
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener("click", () => {
+
+
+    if(body.classList.contains("modo-escuro")) {
+       
+        body.classList.remove("modo-escuro");
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/sun.png");
+
+    } else{
+        
+        body.classList.add("modo-escuro");
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/moon.png");
+    }
+    
+
+
+});
